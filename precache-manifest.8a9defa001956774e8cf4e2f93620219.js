c6918e6f548f437cea3d45eed83fd9c28219fa84
Hassan Abudu <meyian@gmail.com>
2021-03-01 09:38:23 +0000

    Updates

---
+++ precache-manifest.8a9defa001956774e8cf4e2f93620219.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05f4f7ac6ba674f04ee53fe81df519a",
+    "url": "/modern_letters/index.html"
+  },
+  {
+    "revision": "0bdb26dda55c0ab08e5e",
+    "url": "/modern_letters/static/css/main.87e05d0e.chunk.css"
+  },
+  {
+    "revision": "5db1910358aa27b06568",
+    "url": "/modern_letters/static/js/2.226faa51.chunk.js"
+  },
+  {
+    "revision": "7cb18f7ba039a6cd026b3ecb3ba091e2",
+    "url": "/modern_letters/static/js/2.226faa51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bdb26dda55c0ab08e5e",
+    "url": "/modern_letters/static/js/main.ddcbff21.chunk.js"
+  },
+  {
+    "revision": "8b4a3305a77bbf2ab917",
+    "url": "/modern_letters/static/js/runtime-main.0e452342.js"
+  },
+  {
+    "revision": "2e759bf54126f56fe749aa1b2c04fb64",
+    "url": "/modern_letters/static/media/arrow.2e759bf5.png"
+  },
+  {
+    "revision": "25177a8cb359ea4eac53edc3eddcd203",
+    "url": "/modern_letters/static/media/draymond_green.203110.25177a8c.png"
+  },
+  {
+    "revision": "325d1332f7a62518d57b370418d77a03",
+    "url": "/modern_letters/static/media/end.325d1332.png"
+  },
+  {
+    "revision": "e0542d5e255b16d1f3cb42b7d1ffcbd7",
+    "url": "/modern_letters/static/media/home.e0542d5e.png"
+  },
+  {
+    "revision": "cec5d6039843d2463f9df353fd454a75",
+    "url": "/modern_letters/static/media/image1.cec5d603.png"
+  },
+  {
+    "revision": "a46817b4b5158e8a5f95ac8e50b6c267",
+    "url": "/modern_letters/static/media/image2.a46817b4.png"
+  },
+  {
+    "revision": "f01105f87dc7aa2308416e97beafe059",
+    "url": "/modern_letters/static/media/image3.f01105f8.png"
+  },
+  {
+    "revision": "2803ebc05df9b355c24855b249b48464",
+    "url": "/modern_letters/static/media/kevin_durant.201142.2803ebc0.png"
+  },
+  {
+    "revision": "d526af93a9313a451ec4e936a73a10ab",
+    "url": "/modern_letters/static/media/one.d526af93.png"
+  },
+  {
+    "revision": "a6ef98c0d78bf1ea6581a666622acb35",
+    "url": "/modern_letters/static/media/page0.a6ef98c0.png"
+  },
+  {
+    "revision": "f5de3dcf6e53e5961481899b8283a9a9",
+    "url": "/modern_letters/static/media/page1.f5de3dcf.png"
+  },
+  {
+    "revision": "359fbcd6ba6ac4f3ae30332bc11994ad",
+    "url": "/modern_letters/static/media/page2.359fbcd6.png"
+  },
+  {
+    "revision": "f1142b98f0fa02064d70f806e4d8b4c1",
+    "url": "/modern_letters/static/media/scrolling.f1142b98.gif"
+  },
+  {
+    "revision": "acc24c4c0ac0f4c8338743c04f83d7e3",
+    "url": "/modern_letters/static/media/the_view.acc24c4c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1109b4998b6a8b36b5e4cdccbdda5a2f.js"
+  "/modern_letters/precache-manifest.8a9defa001956774e8cf4e2f93620219.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/modern_letters/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
